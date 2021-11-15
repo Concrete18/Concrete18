317bc084308e1049634dcea1ff01598b978f76f4
Concrete18 <michaelericson19@gmail.com>
2021-11-15 15:49:32 -0500

    small changes

--- README.md
+++ README.md
@@ -69,6 +69,8 @@ I began learning AHK for speeding up common tasks that were often completed in a
 
 [![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=Concrete18&repo=Clickr-clone-of-Flickr)](https://github.com/Concrete18/Clickr-clone-of-Flickr)
 
+[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=flow-state-15&repo=discord_clone_2)](https://github.com/flow-state-15/discord_clone_2)
+
 [![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=Concrete18&repo=Good-Games-1-week-Group-Project)](https://github.com/Concrete18/Good-Games-1-week-Group-Project)
 
 [![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=Concrete18&repo=Game-Save-Manager)](https://github.com/Concrete18/Game-Save-Manager)
--- project_creator.py
+++ project_creator.py
@@ -16,50 +16,58 @@ def create_project_buttons(username, projects, format='Markdown') -> None:
     '''
     Creates a text file with your projects set up as markdown or html image links.
 
-    Requires `username` and `projects` to be fiven as arguments.
+    Requires `projects` to be given as arguments.
     '''
-    # checks
-    url = f'https://github-readme-stats.vercel.app/api/pin/?username={username}&repo={projects[0]}'
+    print('Starting README Stat Button Maker')
+    # checks if the api is still working
+    url = f'https://github-readme-stats.vercel.app/api/pin/?username=test_username&repo=test_repo'
     source = requests.get(url)
     if source.status_code != requests.codes.ok:
-        print('Buttons are broken.')
+        print('\nREADME Stat buttons are broken.')
         return
-    # checks
+    else:
+        print('\nREADME Stat buttons are working.')
+    # creates the text file with the selected format
     broken_links = []
-    with open('project_buttons.txt', 'w') as f:
+    with open('project_buttons.md', 'w') as f:
+        # sets up the buttons using HTML
         if format == 'html':
             f.write('<p align="center">\n')
-            for project in projects:
-                if check_repo(username, project):
-                    f.write(f'<a href="https://github.com/{username}/{project}">\n')
-                    f.write(f'<img src="https://github-readme-stats.vercel.app/api/pin/?username={username}&repo={project}" />\n')
+            for name, username in projects.items():
+                if check_repo(username, name):
+                    f.write(f'<a href="https://github.com/{username}/{name}">\n')
+                    f.write(f'<img src="https://github-readme-stats.vercel.app/api/pin/?username={username}&repo={name}" />\n')
                     f.write('<a\>\n')
                 else:
-                    broken_links.append(project)
+                    broken_links.append(name)
             f.write('<p\>')
+        # sets up the buttons using Markdown
         elif 'Markdown':
-            for project in projects:
-                if check_repo(username, project):
-                    image = f'https://github-readme-stats.vercel.app/api/pin/?username={username}&repo={project}'
-                    f.write(f'[![Readme Card]({image})](https://github.com/{username}/{project})\n\n')
+            for name, username in projects.items():
+                if check_repo(username, name):
+                    image = f'https://github-readme-stats.vercel.app/api/pin/?username={username}&repo={name}'
+                    f.write(f'[![Readme Card]({image})](https://github.com/{username}/{name})\n\n')
                 else:
-                    broken_links.append(project)
-    print(broken_links)
+                    broken_links.append(name)
+    print('\nFinished generating template')
+    if len(broken_links) > 0:
+        print('\nThe following repo\'s are invalid.')
+        print(', '.join(broken_links))
 
 
 if __name__ == '__main__':
+    projects_dict = {
+        "Clickr-clone-of-Flickr": "Concrete18",
+        "discord_clone_2": "flow-state-15",
+        "Good-Games-1-week-Group-Project": "Concrete18",
+        "Game-Save-Manager": "Concrete18",
+        "Auto-Folder-Cleaner": "Concrete18",
+        "Config-Auto-Backup": "Concrete18",
+        "Virtual-Assistant": "Concrete18",
+        "Home-Control-Interface": "Concrete18",
+        "Weather-Wallpaper-Changer": "Concrete18",
+        "Standing-Reminder": "Concrete18",
+        "Timed-Shutdown-Sleep": "Concrete18"
+    }
 
-    projects_list = [
-        "Clickr-clone-of-Flickr",
-        "Good-Games-1-week-Group-Project",
-        "Game-Save-Manager",
-        "Auto-Folder-Cleaner",
-        "Config-Auto-Backup",
-        "Virtual-Assistant",
-        "Home-Control-Interface",
-        "Weather-Wallpaper-Changer",
-        "Standing-Reminder",
-        "Timed-Shutdown-Sleep"
-        ]
-
-    create_project_buttons(username='Concrete18', projects=projects_list, format='Markdown')
+    create_project_buttons(username='Concrete18', projects=projects_dict, format='Markdown')
